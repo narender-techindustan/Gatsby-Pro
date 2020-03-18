7e478bf90e7c3639862a408aa6410bbbe9c1505b
narender-techindustan <narender@techindustan.com>
2020-03-18 17:55:57 +0530

    Worked on contact form validation

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -6,34 +6,89 @@ import {
     Col
   } from "react-bootstrap";
 
-const ContactUs = ({ siteTitle }) => (
-    <section>
-        <Container>
-            <Row>
-                <div className="heading text-center">
-                    <h2>Contact us</h2>
-                </div>
-            </Row>
-            <Row>
-                <Col md={4}>
-                    <input type="text" className="form-control" placeholder="Name" />
-                </Col>
-                <Col md={4}>
-                    <input type="text" className="form-control" placeholder="Phone" />
-                </Col>
-                <Col md={4}>
-                    <input type="text" className="form-control" placeholder="Email" />
-                </Col>
-            </Row>
-            <Row>
-                <Col md={12} className="text-center">
-                    <textarea>Message</textarea>
-                    <button type="submit" className="btn-submit">Send</button>
-                </Col>
-            </Row>
-        </Container>
-    </section>
-)
+// const ContactUs = ({ siteTitle }) => (
+export class ContactUs extends React.Component {
+    state = {
+        name: "",
+        phone: "",
+        email: "",
+    }
+    handleInputChange = event => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+          [name]: value,
+        })
+    }
+    handleSubmit = event => {
+        event.preventDefault()
+        alert(`Welcome ${this.state.name} ${this.state.phone} ${this.state.email}!`)
+    }
+    render() {
+        return (
+            <section>
+                <Container>
+                    <Row>
+                        <div className="heading text-center">
+                            <h2>Contact us</h2>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <form onSubmit={this.handleSubmit}>
+                                <Row>
+                                    <Col md={4}>
+                                        <input 
+                                            className="form-control"
+                                            type="text" 
+                                            placeholder="Name"
+                                            name="name"
+                                            value={this.state.name}
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </Col>
+                                    <Col md={4}>
+                                        <input
+                                            className="form-control"
+                                            type="text"
+                                            placeholder="Phone"
+                                            name="phone"
+                                            value={this.state.phone}
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </Col>
+                                    <Col md={4}>
+                                        <input
+                                            className="form-control"
+                                            type="text"
+                                            placeholder="Email" 
+                                            name="email"
+                                            value={this.state.email}
+                                            onChange={this.handleInputChange}    
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={12} className="text-center">
+                                        <textarea>Message</textarea>
+                                        <button
+                                            className="btn-submit"
+                                            type="submit"
+                                        >
+                                            Send
+                                        </button>
+                                    </Col>
+                                </Row>
+                            </form>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        )
+    }
+// )
+}
 
 ContactUs.propTypes = {
   siteTitle: PropTypes.string,
